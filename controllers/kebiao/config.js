cc5152f4d050d573a8d9f0278ff847a638ba83d3
Ling <wssgcg1213@qq.com>
2016-01-25 18:26:24 +0800

    update 20152 config

--- controllers/kebiao/config.js
+++ controllers/kebiao/config.js
@@ -2,8 +2,8 @@
  * Created by Liuchenling on 5/4/15.
  */
 module.exports = {
-	version: "15.9.16",
-    defaultTerm: '2015-2016学年1学期',
+	version: "16.1.25",
+    defaultTerm: '2015-2016学年第2学期',
     mongodbExpire: 2 * 24 * 3600 * 1000,// mongodb 缓存时间
-    termStart: new Date(2015, 8, 7) // 每学期修改，开学第一周的星期一的日子，年，月，日，第二个参数0表示一月份。
+    termStart: new Date(2016, 1, 29) // 每学期修改，开学第一周的星期一的日子，年，月，日，第二个参数0表示一月份。
 };
