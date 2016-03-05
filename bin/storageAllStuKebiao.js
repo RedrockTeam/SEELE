2c097f4fef961aafebfe9fca7995803d1c016f55
Ling <wssgcg1213@qq.com>
2016-03-05 11:33:25 +0800

    finish storageAllStuKebiao

--- bin/storageAllStuKebiao.js
+++ bin/storageAllStuKebiao.js
@@ -1,25 +1,52 @@
 #!/usr/bin/env node
-
+var mongoose = require('mongoose');
 var KebiaoCore = require('../controllers/kebiao/kebiao');
 var KebiaoModel = require('../controllers/kebiao/model');
-var KebiaoConfig = require("../controllers/kebiao/config");
+var config = require('../config');
 
-var map = [
+var stuMap = [
     {start: 2012210001, end: 2012217193},
     {start: 2013210001, end: 2013214501},
     {start: 2014210001, end: 2014214570},
     {start: 2015210001, end: 2015214779}
 ];
 
-map.forEach(function* (chunk) {
-    for (var itor = chunk.start; itor <= chunk.end; itor++) {
-        var data = yield KebiaoCore(xh);
-        if (data && data.success) {
-            var m = new KebiaoModel(data);
-            m.save(function(err){
-                if(err) return console.log(err);
-                console.log('Saved in mongodb', data.stuNum);
-            });
-        }
+function* getKebiao (num) {
+    var data = yield* KebiaoCore(num);
+    if (data && data.success) {
+        var m = new KebiaoModel(data);
+        m.save(function(err){
+            if(err) return console.log(err);
+            console.log('stu', data.stuNum, 'saved');
+        });
     }
+}
+
+mongoose.connect(config.dsn, function (err) {
+    if (err) {
+        console.log("mongodb connect failed, detail:", err);
+        process.exit(1);
+    }
+    console.log("mongodb connect success!");
+
+    stuMap.forEach(function (gap) {
+        (function run(stu, end) {
+            if (stu > end) {
+                console.log("all done");
+                process.exit(0);
+            }
+            var gen = getKebiao(stu);
+
+            function go (result) {
+                if (result.done) {
+                    return run(stu + 1, end);
+                }
+                result.value.then(function (r) {
+                    go(gen.next(r));
+                });
+            }
+
+            go(gen.next());
+        })(gap.start, gap.end);
+    });
 });
